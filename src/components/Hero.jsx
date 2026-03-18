9146fcf5ad9b00e2d3524b84efbf0656e7cfa3c2
Bulani Laknara <bulanilaknara@gmail.com>
2026-03-18 20:40:09 +0530

    Added Hero folder

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+function Hero() {
+  return (
+    <section id="home" className="hero" data-aos="fade-up">
+      <div className="hero-overlay">
+        <div className="container hero-content">
+          <h2>Transform Your Body. Transform Your Life.</h2>
+          <p>Join the ultimate fitness destination</p>
+          <a href="#contact" className="btn btn-primary">
+            Join Now
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
